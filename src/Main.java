12a1f8fe9f02cf7173ef093d2cf87966d332cab1
tiep204 <dangtrantiep240604@gmail.com>
2023-06-30 14:08:29 +0700

    lai

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,18 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String filePath = "D:\\BaiHoc2\\MD-3-JavaCore\\bai16-Io-file\\thuchanh\\th2-maxNumberFiletext\\src\\test.txt";
-        try {
-            String content = readFile(filePath);
-            int wordCount = countWords(content);
-            System.out.println("Số lượng từ trong file: " + wordCount);
-        } catch (IOException e) {
-            System.out.println("Đã xảy ra lỗi khi đọc file: " + e.getMessage());
-        }
-    }
-
-    public static String readFile(String filePath) throws IOException {
-        StringBuilder content = new StringBuilder();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            StringBuilder content = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
                 content.append(line).append("\n");
             }
+            String[] words = content.toString().split("\\s+");
+            int wordCount = words.length;
+            System.out.println("Số lượng từ trong file: " + wordCount);
+        } catch (IOException e) {
+            System.out.println("Đã xảy ra lỗi khi đọc file: " + e.getMessage());
         }
-        return content.toString();
-    }
-
-    public static int countWords(String content) {
-        String[] words = content.split("\\s+");
-        return words.length;
     }
 }
