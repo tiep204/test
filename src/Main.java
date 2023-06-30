e3235f0b68a9f466c8c0838ef300ceffdeb864b3
tiep204 <dangtrantiep240604@gmail.com>
2023-06-30 14:17:22 +0700

    len lai

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String filePath = "D:\\BaiHoc2\\MD-3-JavaCore\\bai16-Io-file\\thuchanh\\th2-maxNumberFiletext\\src\\test.txt";
-
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             StringBuilder content = new StringBuilder();
             String line;
